1c58f79a5e806cf3860d9834fdce8f6585b0310a
Robin Hahling <robin.hahling@gw-computing.net>
2020-02-14 12:14:30 +0100

    container/RingReader: rename Next() to NextFollow(), implement Next()
    In certain situations, it is useful to have a non-blocking `Next()`
    function for the `RingReader`. Thus, rename `Next()` to `NextFollow()`
    and implement and non-blocking `Next()` method.
    
    This new `Next()` method also now inform whether an event has been read
    with success or not and does not require a context to be passed as a
    parameter as this is not necessary for a non-blocking function.
    
    The `Previous()` method also receives the same treatment, ie: it doesn't
    require a context parameter anymore and returns a boolean to indicate a
    successful read.
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- pkg/container/ring_reader_test.go
+++ pkg/container/ring_reader_test.go
@@ -26,7 +26,131 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestRingReader_Previous(t *testing.T) {
+	ring := NewRing(15)
+	for i := 0; i < 15; i++ {
+		ring.Write(&v1.Event{Timestamp: &types.Timestamp{Seconds: int64(i)}})
+	}
+	tests := []struct {
+		start uint64
+		count int
+		want  []*v1.Event
+	}{
+		{
+			start: 13,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 13}},
+			},
+		}, {
+			start: 13,
+			count: 2,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 13}},
+				{Timestamp: &types.Timestamp{Seconds: 12}},
+			},
+		}, {
+			start: 5,
+			count: 5,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 5}},
+				{Timestamp: &types.Timestamp{Seconds: 4}},
+				{Timestamp: &types.Timestamp{Seconds: 3}},
+				{Timestamp: &types.Timestamp{Seconds: 2}},
+				{Timestamp: &types.Timestamp{Seconds: 1}},
+			},
+		}, {
+			start: 0,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 0}},
+			},
+		}, {
+			start: 0,
+			count: 2,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 0}},
+				nil,
+			},
+		}, {
+			start: 14,
+			count: 1,
+			want:  []*v1.Event{nil},
+		},
+	}
+	for _, tt := range tests {
+		name := fmt.Sprintf("read %d, start at position %d", tt.count, tt.start)
+		t.Run(name, func(t *testing.T) {
+			reader := NewRingReader(ring, tt.start)
+			var got []*v1.Event
+			for i := 0; i < tt.count; i++ {
+				got = append(got, reader.Previous())
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
 func TestRingReader_Next(t *testing.T) {
+	ring := NewRing(15)
+	for i := 0; i < 15; i++ {
+		ring.Write(&v1.Event{Timestamp: &types.Timestamp{Seconds: int64(i)}})
+	}
+
+	tests := []struct {
+		start uint64
+		count int
+		want  []*v1.Event
+	}{
+		{
+			start: 0,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 0}},
+			},
+		}, {
+			start: 0,
+			count: 2,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 0}},
+				{Timestamp: &types.Timestamp{Seconds: 1}},
+			},
+		}, {
+			start: 5,
+			count: 5,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 5}},
+				{Timestamp: &types.Timestamp{Seconds: 6}},
+				{Timestamp: &types.Timestamp{Seconds: 7}},
+				{Timestamp: &types.Timestamp{Seconds: 8}},
+				{Timestamp: &types.Timestamp{Seconds: 9}},
+			},
+		}, {
+			start: 13,
+			count: 1,
+			want: []*v1.Event{
+				{Timestamp: &types.Timestamp{Seconds: 13}},
+			},
+		}, {
+			start: 14,
+			count: 1,
+			want:  []*v1.Event{nil},
+		},
+	}
+	for _, tt := range tests {
+		name := fmt.Sprintf("read %d, start at position %d", tt.count, tt.start)
+		t.Run(name, func(t *testing.T) {
+			reader := NewRingReader(ring, tt.start)
+			var got []*v1.Event
+			for i := 0; i < tt.count; i++ {
+				got = append(got, reader.Next())
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestRingReader_NextFollow(t *testing.T) {
 	ring := NewRing(15)
 	for i := 0; i < 15; i++ {
 		ring.Write(&v1.Event{Timestamp: &types.Timestamp{Seconds: int64(i)}})
@@ -82,79 +206,7 @@ func TestRingReader_Next(t *testing.T) {
 			var got []*v1.Event
 			for i := 0; i < tt.count; i++ {
 				ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
-				got = append(got, reader.Next(ctx))
-				select {
-				case <-ctx.Done():
-					timedOut = true
-				default:
-				}
-				cancel()
-			}
-			assert.Equal(t, tt.want, got)
-			assert.Equal(t, tt.wantTimeout, timedOut)
-		})
-	}
-}
-
-func TestRingReader_Previous(t *testing.T) {
-	ring := NewRing(15)
-	for i := 0; i < 15; i++ {
-		ring.Write(&v1.Event{Timestamp: &types.Timestamp{Seconds: int64(i)}})
-	}
-
-	tests := []struct {
-		start       uint64
-		count       int
-		want        []*v1.Event
-		wantTimeout bool
-	}{
-		{
-			start: 13,
-			count: 1,
-			want: []*v1.Event{
-				{Timestamp: &types.Timestamp{Seconds: 13}},
-			},
-		}, {
-			start: 13,
-			count: 2,
-			want: []*v1.Event{
-				{Timestamp: &types.Timestamp{Seconds: 13}},
-				{Timestamp: &types.Timestamp{Seconds: 12}},
-			},
-		}, {
-			start: 5,
-			count: 5,
-			want: []*v1.Event{
-				{Timestamp: &types.Timestamp{Seconds: 5}},
-				{Timestamp: &types.Timestamp{Seconds: 4}},
-				{Timestamp: &types.Timestamp{Seconds: 3}},
-				{Timestamp: &types.Timestamp{Seconds: 2}},
-				{Timestamp: &types.Timestamp{Seconds: 1}},
-			},
-		}, {
-			start: 0,
-			count: 1,
-			want: []*v1.Event{
-				{Timestamp: &types.Timestamp{Seconds: 0}},
-			},
-		}, {
-			start: 0,
-			count: 2,
-			want: []*v1.Event{
-				{Timestamp: &types.Timestamp{Seconds: 0}},
-				nil,
-			},
-		},
-	}
-	for _, tt := range tests {
-		name := fmt.Sprintf("read %d, start at position %d, expect timeout=%t", tt.count, tt.start, tt.wantTimeout)
-		t.Run(name, func(t *testing.T) {
-			reader := NewRingReader(ring, tt.start)
-			var timedOut bool
-			var got []*v1.Event
-			for i := 0; i < tt.count; i++ {
-				ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
-				got = append(got, reader.Previous(ctx))
+				got = append(got, reader.NextFollow(ctx))
 				select {
 				case <-ctx.Done():
 					timedOut = true
