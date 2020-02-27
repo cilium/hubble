0554130fe40331d19d0b12ac7759b5e4238920d7
Robin Hahling <robin.hahling@gw-computing.net>
2020-02-27 14:19:04 +0100

    container: add a priority queue implementation for events
    This priority queue will be used for multi-node support in order to be
    able to re-order events in chronological order as they arrive.
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- pkg/container/ring.go
+++ pkg/container/ring.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package container implements a ring buffer that can be accessed by multiple
-// RingReader readers.
 package container
 
 import (
