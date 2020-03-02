ae69426a13c8e158b94ddb2d8770b39f0b607244
Robin Hahling <robin.hahling@gw-computing.net>
2020-03-02 17:43:34 +0100

    misc: fix copyright owners
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- pkg/server/local_observer.go
+++ pkg/server/local_observer.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Authors of Cilium
+// Copyright 2020 Authors of Hubble
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
