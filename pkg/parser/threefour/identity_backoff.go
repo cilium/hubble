b65b674131c6772e28f87feb5d33bedcedd10e0d
Glib Smaga <code@gsmaga.com>
2020-05-28 14:16:53 -0700

    pkg/parser: Add backoff library for identity fetches
    Signed-off-by: Glib Smaga <code@gsmaga.com>

---
+++ pkg/parser/threefour/identity_backoff.go
@@ -0,0 +1,113 @@
+// Copyright 2020 Authors of Hubble
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package threefour
+
+import (
+	"sync"
+	"time"
+
+	"github.com/cilium/cilium/pkg/backoff"
+	hubtime "github.com/cilium/hubble/pkg/time"
+)
+
+const (
+	defaultBurst      = 25
+	defaultMinBackoff = 100 * time.Millisecond
+	defaultMaxBackoff = 5 * time.Second
+)
+
+func newBackoff() *identityBackoff {
+	return &identityBackoff{
+		burst: defaultBurst,
+		exp: &backoff.Exponential{
+			Min:    defaultMinBackoff,
+			Max:    defaultMaxBackoff,
+			Jitter: true,
+		},
+		bm: make(map[uint64]*tracker),
+	}
+}
+
+// Per-identity request back-off to the Cilium API.
+//
+// Because Cilium IP state is synced only periodically, there is a window of
+// time between ipcache sync where flows will be coming in with what hubble
+// would consider to be invalid ips, or ips that map to old identities. During
+// those times it's important not to flood the cilium API.
+//
+// In general, all this code is a temporary work-around for stand-alone hubble
+// server running on Cilium <=1.7.X. Cilium 1.8+ doesn't have synchronization
+// issues like this as the hubble server is embedded within cilium process.
+type identityBackoff struct {
+	sync.RWMutex
+
+	burst int // do not back-off until burst+1 failed requests
+	exp   *backoff.Exponential
+	bm    map[uint64]*tracker
+}
+
+type tracker struct {
+	attempt     int
+	lastAttempt time.Time // last time it was tried
+}
+
+func (ib *identityBackoff) allowed(i uint64) bool {
+	ib.Lock()
+	defer ib.Unlock()
+
+	tr, ok := ib.bm[i]
+	if !ok {
+		// no backoff is configured for this identity, carry on
+		return true
+	}
+
+	if tr.attempt < ib.burst {
+		// failed attempts are still under burst, allow the request
+		return true
+	}
+
+	// burst has been exhausted for this id, only allow if time passed since
+	// last attempt is allowed by the backoff.
+	nextAllowed := tr.lastAttempt.Add(ib.exp.Duration(tr.attempt - ib.burst))
+	if hubtime.Now().After(nextAllowed) {
+		return true
+	}
+
+	return false
+}
+
+func (ib *identityBackoff) failed(i uint64) {
+	ib.Lock()
+	defer ib.Unlock()
+
+	tr, ok := ib.bm[i]
+	if !ok {
+		tr = &tracker{}
+	}
+
+	tr.attempt++
+	tr.lastAttempt = hubtime.Now()
+
+	ib.bm[i] = tr
+}
+
+func (ib *identityBackoff) clear(i uint64) {
+	ib.Lock()
+	defer ib.Unlock()
+
+	if _, ok := ib.bm[i]; ok {
+		delete(ib.bm, i)
+	}
+}
---
+++ vendor/github.com/cilium/cilium/pkg/backoff/backoff.go
@@ -0,0 +1,134 @@
+// Copyright 2018-2019 Authors of Cilium
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package backoff
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/uuid"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log = logging.DefaultLogger.WithField(logfields.LogSubsys, "backoff")
+
+// NodeManager is the interface required to implement cluster size dependent
+// intervals
+type NodeManager interface {
+	ClusterSizeDependantInterval(baseInterval time.Duration) time.Duration
+}
+
+// Exponential implements an exponential backoff
+type Exponential struct {
+	// Min is the minimal backoff time, if unspecified, 1 second will be
+	// used
+	Min time.Duration
+
+	// Max is the maximum backoff time, if unspecified, no maximum time is
+	// applied
+	Max time.Duration
+
+	// Factor is the factor the backoff time grows exponentially, if
+	// unspecified, a factor of 2.0 will be used
+	Factor float64
+
+	// Jitter, when enabled, adds random jitter to the interval
+	Jitter bool
+
+	// NodeManager enables the use of cluster size dependent backoff
+	// intervals, i.e. the larger the cluster, the longer the backoff
+	// interval
+	NodeManager NodeManager
+
+	// Name is a free form string describing the operation subject to the
+	// backoff, if unspecified, a UUID is generated. This string is used
+	// for logging purposes.
+	Name string
+
+	attempt int
+}
+
+// CalculateDuration calculates the backoff duration based on minimum base
+// interval, exponential factor, jitter and number of failures.
+func CalculateDuration(min, max time.Duration, factor float64, jitter bool, failures int) time.Duration {
+	minFloat := float64(min)
+	maxFloat := float64(max)
+
+	t := minFloat * math.Pow(factor, float64(failures))
+	if max != time.Duration(0) && t > maxFloat {
+		t = maxFloat
+	}
+
+	if jitter {
+		t = rand.Float64()*(t-minFloat) + minFloat
+	}
+
+	return time.Duration(t)
+}
+
+// Wait waits for the required time using an exponential backoff
+func (b *Exponential) Wait(ctx context.Context) error {
+	b.attempt++
+	t := b.Duration(b.attempt)
+
+	log.WithFields(logrus.Fields{
+		"time":    t,
+		"attempt": b.attempt,
+		"name":    b.Name,
+	}).Debug("Sleeping with exponential backoff")
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("exponential backoff cancelled via context: %s", ctx.Err())
+	case <-time.After(t):
+	}
+
+	return nil
+}
+
+// Duration returns the wait duration for the nth attempt
+func (b *Exponential) Duration(attempt int) time.Duration {
+	if b.Name == "" {
+		b.Name = uuid.NewUUID().String()
+	}
+
+	min := time.Duration(1) * time.Second
+	if b.Min != time.Duration(0) {
+		min = b.Min
+	}
+
+	factor := float64(2)
+	if b.Factor != float64(0) {
+		factor = b.Factor
+	}
+
+	t := CalculateDuration(min, b.Max, factor, b.Jitter, attempt)
+
+	if b.NodeManager != nil {
+		t = b.NodeManager.ClusterSizeDependantInterval(t)
+	}
+
+	if b.Max != time.Duration(0) && t > b.Max {
+		t = b.Max
+	}
+
+	return t
+}
