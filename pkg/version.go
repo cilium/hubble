6f0df6ad11c3137f23551535d6ed5af72d98bc3f
Robin Hahling <robin.hahling@gw-computing.net>
2020-05-30 08:22:09 +0200

    set version to 0.7.0-dev
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

--- pkg/version.go
+++ pkg/version.go
@@ -15,7 +15,7 @@
 package pkg
 
 // Version is the software version.
-const Version = "0.6.0-dev"
+const Version = "0.7.0-dev"
 
 // The following variables are set at compile time via LDFLAGS.
 var (
