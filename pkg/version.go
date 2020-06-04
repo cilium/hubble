2068bcaaf708dc7ccb2c37a16e3225ae6e0d3227
Robin Hahling <robin.hahling@gw-computing.net>
2020-06-04 12:09:47 +0200

    make: set hubble version from the Makefile
    This will allow setting the version in the file name of generated
    release binaries
    
    Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- pkg/version.go
+++ pkg/version.go
@@ -14,11 +14,10 @@
 
 package pkg
 
-// Version is the software version.
-const Version = "0.6.0"
-
 // The following variables are set at compile time via LDFLAGS.
 var (
+	// Version is the software version.
+	Version string
 	// GitBranch is the name of the git branch HEAD points to.
 	GitBranch string
 	// GitHash is the git checksum of the most recent commit in HEAD.
