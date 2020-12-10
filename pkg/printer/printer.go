cilium/hubble

commit 94b38afd0a757ed675b7bd83da24b91ce86f6fc4

printer: add basic support for AgentEvent
Support for agent events was added to the Hubble API in
cilium/cilium#14168

Allow to print these messages in the Hubble CLI.

Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

pkg/printer/printer.go
@@ -384,6 +384,170 @@ func (p *Printer) WriteProtoNodeStatusEvent(r *observerpb.GetFlowsResponse) erro
 	return nil
 }
 
+func formatServiceAddr(a *pb.ServiceUpsertNotificationAddr) string {
+	return fmt.Sprintf("%s:%d", a.Ip, a.Port)
+}
+
+func getAgentEventDetails(e *pb.AgentEvent) string {
+	switch e.GetType() {
+	case pb.AgentEventType_AGENT_EVENT_UNKNOWN:
+		if u := e.GetUnknown(); u != nil {
+			return fmt.Sprintf("type: %s, notification: %s", u.Type, u.Notification)
+		}
+	case pb.AgentEventType_AGENT_STARTED:
+		if a := e.GetAgentStart(); a != nil {
+			return fmt.Sprintf("start time: %s", fmtTimestamp(a.Time))
+		}
+	case pb.AgentEventType_POLICY_UPDATED, pb.AgentEventType_POLICY_DELETED:
+		if p := e.GetPolicyUpdate(); p != nil {
+			return fmt.Sprintf("labels: [%s], revision: %d, count: %d",
+				strings.Join(p.Labels, ","), p.Revision, p.RuleCount)
+		}
+	case pb.AgentEventType_ENDPOINT_REGENERATE_SUCCESS, pb.AgentEventType_ENDPOINT_REGENERATE_FAILURE:
+		if r := e.GetEndpointRegenerate(); r != nil {
+			var sb strings.Builder
+			fmt.Fprintf(&sb, "id: %d, labels: [%s]", r.Id, strings.Join(r.Labels, ","))
+			if re := r.Error; re != "" {
+				fmt.Fprintf(&sb, ", error: %s", re)
+			}
+			return sb.String()
+		}
+	case pb.AgentEventType_ENDPOINT_CREATED, pb.AgentEventType_ENDPOINT_DELETED:
+		if ep := e.GetEndpointUpdate(); ep != nil {
+			var sb strings.Builder
+			fmt.Fprintf(&sb, "id: %d", ep.Id)
+			if n := ep.PodName; n != "" {
+				fmt.Fprintf(&sb, ", pod name: %s", n)
+			}
+			if n := ep.Namespace; n != "" {
+				fmt.Fprintf(&sb, ", namespace: %s", n)
+			}
+			return sb.String()
+		}
+	case pb.AgentEventType_IPCACHE_UPSERTED, pb.AgentEventType_IPCACHE_DELETED:
+		if i := e.GetIpcacheUpdate(); i != nil {
+			var sb strings.Builder
+			fmt.Fprintf(&sb, "cidr: %s, identity: %d", i.Cidr, i.Identity)
+			if i.OldIdentity != nil {
+				fmt.Fprintf(&sb, ", old identity: %d", i.OldIdentity.Value)
+			}
+			if i.HostIp != "" {
+				fmt.Fprintf(&sb, ", host ip: %s", i.HostIp)
+			}
+			if i.OldHostIp != "" {
+				fmt.Fprintf(&sb, ", old host ip: %s", i.OldHostIp)
+			}
+			fmt.Fprintf(&sb, ", encrypt key: %d", i.EncryptKey)
+			return sb.String()
+		}
+	case pb.AgentEventType_SERVICE_UPSERTED:
+		if svc := e.GetServiceUpsert(); svc != nil {
+			var sb strings.Builder
+			fmt.Fprintf(&sb, "id: %d", svc.Id)
+			if fe := svc.FrontendAddress; fe != nil {
+				fmt.Fprintf(&sb, ", frontend: %s", formatServiceAddr(fe))
+			}
+			if bes := svc.BackendAddresses; len(bes) != 0 {
+				backends := make([]string, 0, len(bes))
+				for _, a := range bes {
+					backends = append(backends, formatServiceAddr(a))
+				}
+				fmt.Fprintf(&sb, ", backends: [%s]", strings.Join(backends, ","))
+			}
+			if t := svc.Type; t != "" {
+				fmt.Fprintf(&sb, ", type: %s", t)
+			}
+			if tp := svc.TrafficPolicy; tp != "" {
+				fmt.Fprintf(&sb, ", traffic policy: %s", tp)
+			}
+			if n := svc.Name; n != "" {
+				fmt.Fprintf(&sb, ", name: %s", n)
+			}
+			if ns := svc.Namespace; ns != "" {
+				fmt.Fprintf(&sb, ", namespace: %s", ns)
+			}
+			return sb.String()
+		}
+	case pb.AgentEventType_SERVICE_DELETED:
+		if s := e.GetServiceDelete(); s != nil {
+			return fmt.Sprintf("id: %d", s.Id)
+		}
+	}
+	return "UNKNOWN"
+}
+
+// WriteProtoAgentEvent writes v1.AgentEvent into the output writer.
+func (p *Printer) WriteProtoAgentEvent(r *observerpb.GetFlowsResponse) error {
+	e := r.GetAgentEvent()
+	if e == nil {
+		return errors.New("not an agent event")
+	}
+
+	switch p.opts.output {
+	case JSONOutput:
+		return p.jsonEncoder.Encode(e)
+	case JSONPBOutput:
+		return p.jsonEncoder.Encode(r)
+	case DictOutput:
+		ew := &errWriter{w: p.opts.w}
+
+		if p.line != 0 {
+			ew.write(dictSeparator)
+		}
+
+		ew.write("  TIMESTAMP: ", fmtTimestamp(r.GetTime()), newline)
+		if p.opts.nodeName {
+			ew.write("       NODE: ", r.GetNodeName(), newline)
+		}
+		ew.write(
+			"       TYPE: ", e.GetType(), newline,
+			"    DETAILS: ", getAgentEventDetails(e), newline,
+		)
+		if ew.err != nil {
+			return fmt.Errorf("failed to write out agent event: %v", ew.err)
+		}
+	case TabOutput:
+		ew := &errWriter{w: p.tw}
+		if p.line == 0 {
+			ew.write("TIMESTAMP", tab)
+			if p.opts.nodeName {
+				ew.write("NODE", tab)
+			}
+			ew.write(
+				"TYPE", tab,
+				"DETAILS", newline,
+			)
+		}
+		ew.write(fmtTimestamp(r.GetTime()), tab)
+		if p.opts.nodeName {
+			ew.write(r.GetNodeName(), tab)
+		}
+		ew.write(
+			e.GetType(), tab,
+			getAgentEventDetails(e), newline,
+		)
+		if ew.err != nil {
+			return fmt.Errorf("failed to write out agent event: %v", ew.err)
+		}
+	case CompactOutput:
+		var node string
+
+		if p.opts.nodeName {
+			node = fmt.Sprintf(" [%s]", r.GetNodeName())
+		}
+		_, err := fmt.Fprintf(p.opts.w,
+			"%s%s: %s\n",
+			fmtTimestamp(r.GetTime()),
+			node,
+			e.GetType())
+		if err != nil {
+			return fmt.Errorf("failed to write out agent event: %v", err)
+		}
+	}
+	p.line++
+	return nil
+}
+
 // MaybeTime returns a Millisecond precision timestamp, or "N/A" if nil.
 func MaybeTime(t *time.Time) string {
 	if t != nil {
@@ -425,6 +589,8 @@ func (p *Printer) WriteGetFlowsResponse(res *observerpb.GetFlowsResponse) error 
 		return p.WriteProtoFlow(res)
 	case *observerpb.GetFlowsResponse_NodeStatus:
 		return p.WriteProtoNodeStatusEvent(res)
+	case *observerpb.GetFlowsResponse_AgentEvent:
+		return p.WriteProtoAgentEvent(res)
 	case nil:
 		return nil
 	default:

pkg/printer/printer_test.go
@@ -21,7 +21,9 @@ import (
 	"time"
 
 	monitorAPI "github.com/cilium/cilium/pkg/monitor/api"
+	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -629,4 +631,202 @@ func TestHostname(t *testing.T) {
 	assert.Equal(t, "1.1.1.1:80", p.Hostname("1.1.1.1", "80", "default", "pod", "service", []string{}))
 	assert.Equal(t, "1.1.1.1", p.Hostname("1.1.1.1", "0", "default", "pod", "", []string{}))
 	assert.Equal(t, "1.1.1.1", p.Hostname("1.1.1.1", "0", "default", "pod", "service", []string{}))
+}
+
+func TestPrinter_AgentEventDetails(t *testing.T) {
+	startTS, err := ptypes.TimestampProto(time.Now())
+	assert.NoError(t, err)
+
+	tests := []struct {
+		name string
+		ev   *pb.AgentEvent
+		want string
+	}{
+		{
+			name: "nil",
+			want: "UNKNOWN",
+		},
+		{
+			name: "empty",
+			ev:   &pb.AgentEvent{},
+			want: "UNKNOWN",
+		},
+		{
+			name: "unknown without notification",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_AGENT_EVENT_UNKNOWN,
+			},
+			want: "UNKNOWN",
+		},
+		{
+			name: "agent start without notification",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_AGENT_STARTED,
+			},
+			want: "UNKNOWN",
+		},
+		{
+			name: "agent start with notification",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_AGENT_STARTED,
+				Notification: &pb.AgentEvent_AgentStart{
+					AgentStart: &pb.TimeNotification{
+						Time: startTS,
+					},
+				},
+			},
+			want: "start time: " + fmtTimestamp(startTS),
+		},
+		{
+			name: "policy update",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_POLICY_UPDATED,
+				Notification: &pb.AgentEvent_PolicyUpdate{
+					PolicyUpdate: &pb.PolicyUpdateNotification{
+						Labels:    []string{"foo=bar", "baz=foo"},
+						Revision:  1,
+						RuleCount: 2,
+					},
+				},
+			},
+			want: "labels: [foo=bar,baz=foo], revision: 1, count: 2",
+		},
+		{
+			name: "policy delete",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_POLICY_DELETED,
+				Notification: &pb.AgentEvent_PolicyUpdate{
+					PolicyUpdate: &pb.PolicyUpdateNotification{
+						Revision:  42,
+						RuleCount: 1,
+					},
+				},
+			},
+			want: "labels: [], revision: 42, count: 1",
+		},
+		{
+			name: "endpoint regenerate success",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_ENDPOINT_REGENERATE_SUCCESS,
+				Notification: &pb.AgentEvent_EndpointRegenerate{
+					EndpointRegenerate: &pb.EndpointRegenNotification{
+						Id:     42,
+						Labels: []string{"baz=bar", "some=label"},
+					},
+				},
+			},
+			want: "id: 42, labels: [baz=bar,some=label]",
+		},
+		{
+			name: "endpoint regenerate failure",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_ENDPOINT_REGENERATE_FAILURE,
+				Notification: &pb.AgentEvent_EndpointRegenerate{
+					EndpointRegenerate: &pb.EndpointRegenNotification{
+						Id:     42,
+						Labels: []string{"baz=bar", "some=label"},
+						Error:  "some error",
+					},
+				},
+			},
+			want: "id: 42, labels: [baz=bar,some=label], error: some error",
+		},
+		{
+			name: "endpoint created",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_ENDPOINT_CREATED,
+				Notification: &pb.AgentEvent_EndpointUpdate{
+					EndpointUpdate: &pb.EndpointUpdateNotification{
+						Id:        1027,
+						PodName:   "cilium-xyz",
+						Namespace: "kube-system",
+					},
+				},
+			},
+			want: "id: 1027, pod name: cilium-xyz, namespace: kube-system",
+		},
+		{
+			name: "ipcache upsert",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_IPCACHE_UPSERTED,
+				Notification: &pb.AgentEvent_IpcacheUpdate{
+					IpcacheUpdate: &pb.IPCacheNotification{
+						Cidr:     "10.1.2.3/32",
+						Identity: 42,
+						OldIdentity: &wrappers.UInt32Value{
+							Value: 23,
+						},
+						HostIp:     "192.168.3.9",
+						EncryptKey: 3,
+					},
+				},
+			},
+			want: "cidr: 10.1.2.3/32, identity: 42, old identity: 23, host ip: 192.168.3.9, encrypt key: 3",
+		},
+		{
+			name: "ipcache delete",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_IPCACHE_DELETED,
+				Notification: &pb.AgentEvent_IpcacheUpdate{
+					IpcacheUpdate: &pb.IPCacheNotification{
+						Cidr:      "10.0.1.2/32",
+						Identity:  42,
+						OldHostIp: "192.168.1.23",
+					},
+				},
+			},
+			want: "cidr: 10.0.1.2/32, identity: 42, old host ip: 192.168.1.23, encrypt key: 0",
+		},
+		{
+			name: "service upsert",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_SERVICE_UPSERTED,
+				Notification: &pb.AgentEvent_ServiceUpsert{
+					ServiceUpsert: &pb.ServiceUpsertNotification{
+						Id: 42,
+						FrontendAddress: &pb.ServiceUpsertNotificationAddr{
+							Ip:   "10.0.0.42",
+							Port: 8008,
+						},
+						BackendAddresses: []*pb.ServiceUpsertNotificationAddr{
+							{
+								Ip:   "192.168.1.23",
+								Port: 80,
+							},
+							{
+								Ip:   "192.168.1.24",
+								Port: 80,
+							},
+						},
+						Type:          "foobar",
+						TrafficPolicy: "pol1",
+						Name:          "foo",
+						Namespace:     "bar",
+					},
+				},
+			},
+			want: "id: 42, frontend: 10.0.0.42:8008, backends: [192.168.1.23:80,192.168.1.24:80], type: foobar, traffic policy: pol1, name: foo, namespace: bar",
+		},
+		{
+			name: "service delete",
+			ev: &pb.AgentEvent{
+				Type: pb.AgentEventType_SERVICE_DELETED,
+				Notification: &pb.AgentEvent_ServiceDelete{
+					ServiceDelete: &pb.ServiceDeleteNotification{
+						Id: 42,
+					},
+				},
+			},
+			want: "id: 42",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getAgentEventDetails(tt.ev); got != tt.want {
+				t.Errorf("getAgentEventDetails()\ngot:  %v,\nwant: %v", got, tt.want)
+			}
+		})
+	}
+
 }

commit dec64592a9ee5d178d738c7ebe0fdc5b113dc5a1

printer: print namespace before pod/service name
Addresses https://github.com/cilium/hubble/pull/442#discussion_r539178607

Fixes: 94b38afd0a75 ("printer: add basic support for AgentEvent")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

pkg/printer/printer.go
@@ -416,11 +416,11 @@ func getAgentEventDetails(e *pb.AgentEvent) string {
 		if ep := e.GetEndpointUpdate(); ep != nil {
 			var sb strings.Builder
 			fmt.Fprintf(&sb, "id: %d", ep.Id)
-			if n := ep.PodName; n != "" {
-				fmt.Fprintf(&sb, ", pod name: %s", n)
-			}
 			if n := ep.Namespace; n != "" {
 				fmt.Fprintf(&sb, ", namespace: %s", n)
+			}
+			if n := ep.PodName; n != "" {
+				fmt.Fprintf(&sb, ", pod name: %s", n)
 			}
 			return sb.String()
 		}
@@ -460,11 +460,11 @@ func getAgentEventDetails(e *pb.AgentEvent) string {
 			if tp := svc.TrafficPolicy; tp != "" {
 				fmt.Fprintf(&sb, ", traffic policy: %s", tp)
 			}
-			if n := svc.Name; n != "" {
-				fmt.Fprintf(&sb, ", name: %s", n)
-			}
 			if ns := svc.Namespace; ns != "" {
 				fmt.Fprintf(&sb, ", namespace: %s", ns)
+			}
+			if n := svc.Name; n != "" {
+				fmt.Fprintf(&sb, ", name: %s", n)
 			}
 			return sb.String()
 		}

pkg/printer/printer_test.go
@@ -738,12 +738,12 @@ func TestPrinter_AgentEventDetails(t *testing.T) {
 				Notification: &pb.AgentEvent_EndpointUpdate{
 					EndpointUpdate: &pb.EndpointUpdateNotification{
 						Id:        1027,
-						PodName:   "cilium-xyz",
 						Namespace: "kube-system",
+						PodName:   "cilium-xyz",
 					},
 				},
 			},
-			want: "id: 1027, pod name: cilium-xyz, namespace: kube-system",
+			want: "id: 1027, namespace: kube-system, pod name: cilium-xyz",
 		},
 		{
 			name: "ipcache upsert",
@@ -800,12 +800,12 @@ func TestPrinter_AgentEventDetails(t *testing.T) {
 						},
 						Type:          "foobar",
 						TrafficPolicy: "pol1",
-						Name:          "foo",
 						Namespace:     "bar",
+						Name:          "foo",
 					},
 				},
 			},
-			want: "id: 42, frontend: 10.0.0.42:8008, backends: [192.168.1.23:80,[2001:db8:85a3:::8a2e:370:1337]:8080], type: foobar, traffic policy: pol1, name: foo, namespace: bar",
+			want: "id: 42, frontend: 10.0.0.42:8008, backends: [192.168.1.23:80,[2001:db8:85a3:::8a2e:370:1337]:8080], type: foobar, traffic policy: pol1, namespace: bar, name: foo",
 		},
 		{
 			name: "service delete",