cilium/hubble

commit 099da23a07b008fdc7d1524631b4968eeeef5011

printer: ommit node name from output
compact, dict, table: node name hidden by default, can be displayed with --print-node-name
json: always print all fields (as is the case now)

Signed-off-by: Marco De Luca <marcodl404@gmail.com>

pkg/printer/printer.go
@@ -18,6 +18,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"path"
@@ -41,6 +42,18 @@ type Printer struct {
 	line        int
 	tw          *tabwriter.Writer
 	jsonEncoder *json.Encoder
+}
+
+type errWriter struct {
+	w   io.Writer
+	err error
+}
+
+func (ew *errWriter) write(a ...interface{}) {
+	if ew.err != nil {
+		return
+	}
+	_, ew.err = fmt.Fprint(ew.w, a...)
 }
 
 // New Printer.
@@ -192,67 +205,80 @@ func GetFlowType(f v1.Flow) string {
 // WriteProtoFlow writes v1.Flow into the output writer.
 func (p *Printer) WriteProtoFlow(res *observerpb.GetFlowsResponse) error {
 	f := res.GetFlow()
+
 	switch p.opts.output {
 	case TabOutput:
+		ew := &errWriter{w: p.tw}
+		src, dst := p.GetHostNames(f)
+
 		if p.line == 0 {
-			_, err := fmt.Fprint(p.tw,
-				"TIMESTAMP", tab,
+			ew.write("TIMESTAMP", tab)
+			if p.opts.nodeName {
+				ew.write("NODE", tab)
+			}
+			ew.write(
 				"SOURCE", tab,
 				"DESTINATION", tab,
 				"TYPE", tab,
 				"VERDICT", tab,
 				"SUMMARY", newline,
 			)
-			if err != nil {
-				return err
-			}
 		}
-		src, dst := p.GetHostNames(f)
-		_, err := fmt.Fprint(p.tw,
-			fmtTimestamp(f.GetTime()), tab,
+		ew.write(fmtTimestamp(f.GetTime()), tab)
+		if p.opts.nodeName {
+			ew.write(f.GetNodeName(), tab)
+		}
+		ew.write(
 			src, tab,
 			dst, tab,
 			GetFlowType(f), tab,
 			f.GetVerdict().String(), tab,
 			f.GetSummary(), newline,
 		)
-		if err != nil {
-			return fmt.Errorf("failed to write out packet: %v", err)
+		if ew.err != nil {
+			return fmt.Errorf("failed to write out packet: %v", ew.err)
 		}
 	case DictOutput:
+		ew := &errWriter{w: p.opts.w}
+		src, dst := p.GetHostNames(f)
+
 		if p.line != 0 {
 			// TODO: line length?
-			_, err := fmt.Fprintln(p.opts.w, dictSeparator)
-			if err != nil {
-				return err
-			}
+			ew.write(dictSeparator)
 		}
-		src, dst := p.GetHostNames(f)
+
 		// this is a little crude, but will do for now. should probably find the
 		// longest header and auto-format the keys
-		_, err := fmt.Fprint(p.opts.w,
-			"  TIMESTAMP: ", fmtTimestamp(f.GetTime()), newline,
+		ew.write("  TIMESTAMP: ", fmtTimestamp(f.GetTime()), newline)
+		if p.opts.nodeName {
+			ew.write("       NODE: ", f.GetNodeName(), newline)
+		}
+		ew.write(
 			"     SOURCE: ", src, newline,
 			"DESTINATION: ", dst, newline,
 			"       TYPE: ", GetFlowType(f), newline,
 			"    VERDICT: ", f.GetVerdict().String(), newline,
 			"    SUMMARY: ", f.GetSummary(), newline,
 		)
-		if err != nil {
-			return fmt.Errorf("failed to write out packet: %v", err)
+		if ew.err != nil {
+			return fmt.Errorf("failed to write out packet: %v", ew.err)
 		}
 	case CompactOutput:
+		var node string
 		src, dst := p.GetHostNames(f)
+
+		if p.opts.nodeName {
+			node = fmt.Sprintf(" [%s]", f.GetNodeName())
+		}
 		_, err := fmt.Fprintf(p.opts.w,
-			"%s [%s]: %s -> %s %s %s (%s)\n",
+			"%s%s: %s -> %s %s %s (%s)\n",
 			fmtTimestamp(f.GetTime()),
-			f.GetNodeName(),
+			node,
 			src,
 			dst,
 			GetFlowType(f),
 			f.GetVerdict().String(),
-			f.GetSummary(),
-		)
+			f.GetSummary())
 		if err != nil {
 			return fmt.Errorf("failed to write out packet: %v", err)
 		}

cmd/observe/observe.go
@@ -49,6 +49,7 @@ var (
 	follow              bool
 	ignoreStderr        bool
 	enableIPTranslation bool
+	nodeName            bool
 
 	printer *hubprinter.Printer
 
@@ -242,6 +243,8 @@ programs attached to endpoints and devices. This includes:
 		"Translate IP addresses to logical names such as pod name, FQDN, ...",
 	)
 
+	observerCmd.Flags().BoolVarP(&nodeName, "print-node-name", "", false, "Print node name in output")
+
 	customObserverHelp(observerCmd)
 
 	return observerCmd
@@ -303,6 +306,9 @@ func handleArgs(ofilter *observeFilter) (err error) {
 	}
 	if debug {
 		opts = append(opts, hubprinter.WithDebug())
+	}
+	if nodeName {
+		opts = append(opts, hubprinter.WithNodeName())
 	}
 	printer = hubprinter.New(opts...)
 	return nil

pkg/printer/options.go
@@ -43,6 +43,7 @@ type Options struct {
 	werr                io.Writer
 	enableDebug         bool
 	enableIPTranslation bool
+	nodeName            bool
 }
 
 // Option ...
@@ -108,5 +109,12 @@ func WithDebug() Option {
 func WithIPTranslation() Option {
 	return func(opts *Options) {
 		opts.enableIPTranslation = true
+	}
+}
+
+// WithNodeName enables printing the node name.
+func WithNodeName() Option {
+	return func(opts *Options) {
+		opts.nodeName = true
 	}
 }

pkg/printer/printer_test.go
@@ -80,6 +80,19 @@ func TestPrinter_WriteProtoFlow(t *testing.T) {
 Jan  1 00:20:34.567   1.1.1.1:31793   2.2.2.2:8080   Policy denied   DROPPED   TCP Flags: SYN`,
 		},
 		{
+			name: "tabular-with-node",
+			options: []Option{
+				WithNodeName(),
+				Writer(&buf),
+			},
+			args: args{
+				f: &f,
+			},
+			wantErr: false,
+			expected: `TIMESTAMP             NODE   SOURCE          DESTINATION    TYPE            VERDICT   SUMMARY
+Jan  1 00:20:34.567   k8s1   1.1.1.1:31793   2.2.2.2:8080   Policy denied   DROPPED   TCP Flags: SYN`,
+		},
+		{
 			name: "compact",
 			options: []Option{
 				Compact(),
@@ -89,8 +102,23 @@ Jan  1 00:20:34.567   1.1.1.1:31793   2.2.2.2:8080   Policy denied   DROPPED   T
 				f: &f,
 			},
 			wantErr: false,
-			expected: "Jan  1 00:20:34.567 " +
-				"[k8s1]: 1.1.1.1:31793 -> 2.2.2.2:8080 " +
+			expected: "Jan  1 00:20:34.567: " +
+				"1.1.1.1:31793 -> 2.2.2.2:8080 " +
+				"Policy denied DROPPED (TCP Flags: SYN)\n",
+		},
+		{
+			name: "compact-with-node",
+			options: []Option{
+				Compact(),
+				WithNodeName(),
+				Writer(&buf),
+			},
+			args: args{
+				f: &f,
+			},
+			wantErr: false,
+			expected: "Jan  1 00:20:34.567 [k8s1]: " +
+				"1.1.1.1:31793 -> 2.2.2.2:8080 " +
 				"Policy denied DROPPED (TCP Flags: SYN)\n",
 		},
 		{
@@ -138,6 +166,25 @@ Jan  1 00:20:34.567   1.1.1.1:31793   2.2.2.2:8080   Policy denied   DROPPED   T
 			},
 			wantErr: false,
 			expected: `  TIMESTAMP: Jan  1 00:20:34.567
+     SOURCE: 1.1.1.1:31793
+DESTINATION: 2.2.2.2:8080
+       TYPE: Policy denied
+    VERDICT: DROPPED
+    SUMMARY: TCP Flags: SYN`,
+		},
+		{
+			name: "dict-with-node",
+			options: []Option{
+				Dict(),
+				WithNodeName(),
+				Writer(&buf),
+			},
+			args: args{
+				f: &f,
+			},
+			wantErr: false,
+			expected: `  TIMESTAMP: Jan  1 00:20:34.567
+       NODE: k8s1
      SOURCE: 1.1.1.1:31793
 DESTINATION: 2.2.2.2:8080
        TYPE: Policy denied

commit f841465bf977e92b277047613195c5f0c7d8a00b

printer: fix dict outout newline
Commit 099da23a07b008fdc7d1524631b4968eeeef5011 broke the dict output by
dropping the newline after the flow separator. This commit adds back the
missing newline.

Signed-off-by: Robin Hahling <robin.hahling@gw-computing.net>

pkg/printer/printer.go
@@ -263,7 +263,7 @@ func (p *Printer) WriteProtoFlow(res *observerpb.GetFlowsResponse) error {
 
 		if p.line != 0 {
 			// TODO: line length?
-			ew.write(dictSeparator)
+			ew.write(dictSeparator, newline)
 		}
 
 		// this is a little crude, but will do for now. should probably find the