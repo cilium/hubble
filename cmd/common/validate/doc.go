409a6ff1014de8f77be0d4bb4ad9bbde55375685
Tobias Klauser <tobias@cilium.io>
2022-11-09 15:36:31 +0100

    all: remove year from copyright notice
    Hubble source files have a header with a SPDX tag and a copyright
    notice, stating that the copyright goes to the Authors of Hubble, and
    containing a year or year range to specify to what period the copyright
    extends.
    
    The year or year range are not necessary in this statement. The CNCF
    recommends not using the year or the copyright symbol, to keep files
    easy to process [0]. The history of the source code is conserved through
    the version control system, and is easily available for all to check
    out.
    
    [0] https://github.com/cncf/foundation/blob/main/copyright-notices.md#copyright-notices
    
    Drop the year and year range for the entire codebase.
    
    The files were processed as follows:
    
    $ cat years.sed
    6, $ b
    /^\(#\|\/\/\|\/\*\) *SPDX-License-Identifier: \(GPL-2\.0\|Apache-2\.0\)\( \*\/\)\?$/ {
        N; s/Copyright .* Authors of Hubble/Copyright Authors of Hubble/
    }
    /^\(#\|\/\/\|\/\*\) *Copyright .* Authors of Hubble\( \*\/\)\?$/ {
        N; {
    	/\(#\|\/\/\|\/\*\) *SPDX-License-Identifier: \(GPL-2\.0\|Apache-2\.0\)\( \*\/\)\?$/ s/Copyright .* Authors of Hubble/Copyright Authors of Hubble/
        }
    }
    $ find main.go cmd pkg -type f -print0 | xargs -0 -P 8 sed -i -f years.sed
    
    Also add copyright headers to some Go source files that were previously
    missing the headers.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cmd/common/validate/doc.go
+++ cmd/common/validate/doc.go
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// Copyright 2020 Authors of Hubble
+// Copyright Authors of Hubble
 
 // Package validate implements utilities to validate the command configuration
 // by, for example, validating a set of given options.
