b421e17aaa2928b042ce69ecfd9d5a48147b9f50
Alexandre Perrin <alex@isovalent.com>
2023-07-06 11:42:47 +0200

    observe: fix --from-identity and --to-identity
    Before this patch, --{from,to}-identity would not work as expected. The
    identity filter was added to only one side of the left/right filter, and
    thus would not be taken into account:
    
        % hubble observe --from-pod cilium --to-identity world --print-raw-filters
        allowlist:
            - '{"source_pod":["cilium"]}'
            - '{"source_pod":["cilium"],"destination_identity":[2]}'
    
    The identity filter is added only to the allowlist's right/destination
    filter, and since the allowlist's left/source filter is a superset of
    the right filter, the resulting allowlist is equivalent to:
    
        % hubble observe --from-pod cilium --print-raw-filters
        allowlist:
            - '{"source_pod":["cilium"]}'
    
    Another example where the combined flag uses both left and right
    filters:
    
        % hubble observe --pod cilium --to-identity world --print-raw-filters
        allowlist:
        - '{"source_pod":["cilium"]}'
        - '{"destination_pod":["cilium"],"destination_identity":[2]}'
    
    The right filter will never match and the left filter is again
    equivalent to the one we get without `--to-identity world`.
    
    The --from-identity filter had the same bug, but was added the only the
    left filter instead of the right filter.
    
    This patch ensure that --{from,to}-identity filters are added on both
    sides, and adds a regression test case. After this patch we get:
    
        % hubble observe --from-pod cilium --to-identity world --print-raw-filters
        allowlist:
        - '{"source_pod":["cilium"],"destination_identity":[2]}'
    
        % hubble observe --pod cilium --to-identity world --print-raw-filters
        allowlist:
        - '{"source_pod":["cilium"],"destination_identity":[2]}'
        - '{"destination_pod":["cilium"],"destination_identity":[2]}'
    
    Signed-off-by: Alexandre Perrin <alex@isovalent.com>

--- cmd/observe/flows_filter.go
+++ cmd/observe/flows_filter.go
@@ -529,7 +529,7 @@ func (of *flowFilter) set(f *filterTracker, name, val string, track bool) error 
 		if err != nil {
 			return fmt.Errorf("invalid security identity, expected one of %v or a numeric value", reservedIdentitiesNames())
 		}
-		f.applyLeft(func(f *flowpb.FlowFilter) {
+		f.apply(func(f *flowpb.FlowFilter) {
 			f.SourceIdentity = append(f.SourceIdentity, identity.Uint32())
 		})
 	case "to-identity":
@@ -537,7 +537,7 @@ func (of *flowFilter) set(f *filterTracker, name, val string, track bool) error 
 		if err != nil {
 			return fmt.Errorf("invalid security identity, expected one of %v or a numeric value", reservedIdentitiesNames())
 		}
-		f.applyRight(func(f *flowpb.FlowFilter) {
+		f.apply(func(f *flowpb.FlowFilter) {
 			f.DestinationIdentity = append(f.DestinationIdentity, identity.Uint32())
 		})
 
